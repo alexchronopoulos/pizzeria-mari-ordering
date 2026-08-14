c20aa2d244829dc4b21875c76ec43a839d937480
Alex <alex@alxit.net>
2026-08-13 22:07:46 -0400

    Fix mobile ordering issue again

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from app.startup_performance import prepare_app_for_serving
 
 
 app = create_app()
-prepare_app_for_serving(app, version="0.18.24")
+prepare_app_for_serving(app, version="0.18.25")
 
 
 if __name__ == "__main__":
